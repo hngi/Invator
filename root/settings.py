465257f1daba41d7e0a0fb9ecbae1f824f52e587
believemanasseh <believemanasseh@gmail.com>
2020-07-31 11:58:03 +0100

    deploy on heroku

--- root/settings.py
+++ root/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -91,17 +90,12 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if ENV:
-    DATABASES = {
-        'default': {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    }
-else:
-    DATABASES = dict()
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+}
 
 AUTHENTICATION_BACKENDS = [
 
@@ -146,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
