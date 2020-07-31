17a5813e8e34722fb0c4c20d3235857f71fdaaa2
believemanasseh <believemanasseh@gmail.com>
2020-08-01 00:44:08 +0100

    changed debug to false

--- root/settings.py
+++ root/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q_rurh)c0jkcqt2wu^fi1t^&vq#rn0!#f39)5o4@77iaq!vz^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'invator.herokuapp.com']
 
