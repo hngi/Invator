66c859c110605fa07c1d6ed86c36547924859562
Clashkid155 <frankdiss9@gmail.com>
2020-07-31 19:03:04 +0100

    Add Create invoice (beta)

--- invator/views.py
+++ invator/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from weasyprint import HTML
 import tempfile
-from django.http import Http404, HttpResponse
-from .models import Invoice
+from django.http import Http404, HttpResponse, JsonResponse
+from django.contrib.auth.models import User, auth
+from .models import Invoice, Transaction
 from django.db.models import Sum, F
 from datetime import datetime
 from django.conf import settings
@@ -66,3 +67,42 @@ def dashboard(request):
         context = order_invoice[:4]
         return render(request, "dashboard.html", {'data': context})
     return render(request, "dashboard.html")
+
+def invoice(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            user = request.user
+            #role = request.POST["title"]
+            item = request.POST["item"]
+            #invoice_nam = request.POST["name"] or None
+            price = request.POST["price"]
+            quantity = request.POST["quantity"]
+            #total = request.POST["total"] or None
+            to_full_name = request.POST["to_name"]
+            #bank_name = request.POST["bank_name"]
+            to_address = request.POST["to_address"]
+            #account_name = request.POST["account_name"]
+            account_number = request.POST["account_number"]
+            from_phone = request.POST["from_phone"]
+            from_full_name = request.POST["from_full_name"]
+            from_address = request.POST["from_address"]
+            to_phone = request.POST["to_phone"]
+
+            #tran = Transaction.objects.create(price=price, item=item, quantity=quantity, total=1)
+            #tran.save()
+
+            xo = Invoice.objects.create(user=user, to_phone=to_phone,
+                    to_address=to_address, account_number=account_number,
+                    from_full_name=from_full_name, from_phone=from_phone,
+                    to_full_name=to_full_name)
+            xo.save()
+
+            # Can't save transactions. Dom't know why
+            xo.transactions.create(price=price, item=item, quantity=quantity, total=1)
+            xo.save()
+
+            return render(request, "dashboard.html")
+
+
+    return render(request, "invoice-gen.html")
+
